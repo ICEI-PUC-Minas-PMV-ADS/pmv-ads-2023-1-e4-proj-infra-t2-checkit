3029f6196bb9fc6a04ed21dfaa14d33ab41c474b
Ana Paula Buchholz <anapbuchholz@gmail.com>
2023-03-26 22:53:26 -0300

    [feature] Creating controller folder

---
+++ src/ProjectManager/ProjectManager/Controllers/ProjectsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectManager.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectsController : ControllerBase
+    {
+    }
+}
