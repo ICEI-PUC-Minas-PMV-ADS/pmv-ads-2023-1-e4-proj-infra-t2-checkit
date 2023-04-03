2010ea69652f65eb594c8e838818506112aad298
Gabriel Lopes <gabrielpuneco@gmail.com>
2023-04-02 21:35:56 -0300

    [feature]Implementação do get all tarefas,e criação do serviço de Histórico de Tarefas e Projetos

---
+++ src/Task&ProjectHistory/Task&ProjectHistory/Task&ProjectHistory/Models/Historic.cs
@@ -0,0 +1,19 @@
+﻿namespace Task_ProjectHistory.Models
+{
+    public class Historic
+    {
+        public string Id { get; set; }
+
+        public string TituloTarefa { get; set; }
+
+        public string TituloProjeto { get; set; }
+
+        public DateTime DataConclusao { get; set; }
+
+
+
+
+
+
+    }
+}
---
+++ src/Task&ProjectHistory/Task&ProjectHistory/Task&ProjectHistory/Models/HistoricDatabaseSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace Task_ProjectHistory.Models
+{
+    public class HistoricDatabaseSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+        public string DatabaseName { get; set; } = null!;
+        public string HistoricCollectionName { get; set; } = null!;
+
+
+
+    }
+}
