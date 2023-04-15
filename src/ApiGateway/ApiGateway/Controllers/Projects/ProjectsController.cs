bfb355437ff3352f8a10fac9aa71b37323e57334
Gabriel Lopes <gabrielpuneco@gmail.com>
2023-04-15 11:51:49 -0300

    [feature] Implementando o PostProject na ApiGateway

--- src/ApiGateway/ApiGateway/Controllers/Projects/ProjectsController.cs
+++ src/ApiGateway/ApiGateway/Controllers/Projects/ProjectsController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ApiGateway.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace ApiGateway.Controllers.Projects
 {
@@ -19,6 +22,17 @@ namespace ApiGateway.Controllers.Projects
            var result = await _httpClient.GetAsync($"https://localhost:7152/api/Projects/{id}");
 
             return Ok(result);
+        }
+        [HttpPost()]
+
+        public async Task<IActionResult> PostProject([FromBody] Project project)
+        {
+            string json = JsonConvert.SerializeObject(project);
+            StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+        
+            var p1 = await _httpClient.PostAsync($"https://localhost:7152/api/Projects", content);
+
+            return Ok(p1);
         }
     }
 }
