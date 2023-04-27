b1faea313c1d2bf8f229cfe79792341fc89fd898
Gabriel Lopes <gabrielpuneco@gmail.com>
2023-04-27 18:30:36 -0300

    Correção erro babel

--- MobileApp/babel.config.js
+++ MobileApp/babel.config.js
@@ -2,11 +2,10 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-<<<<<<< HEAD
-=======
+
+
     plugins: [      
       'react-native-reanimated/plugin',
     ],
->>>>>>> 2f1a48edd80a4844d51402f32a77f780a536d89e
   };
 };
