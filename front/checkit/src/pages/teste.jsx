e729e76d9c1625c8fb3f99f2d4a24607246eb189
Julia Sartori <juliasartori@outlook.com.br>
2023-05-11 23:24:35 +0200

    testing routing

---
+++ front/checkit/src/pages/teste.jsx
@@ -0,0 +1,10 @@
+function Teste() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>teste</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
+    </div>
+  );
+}
+
+export default Teste;
