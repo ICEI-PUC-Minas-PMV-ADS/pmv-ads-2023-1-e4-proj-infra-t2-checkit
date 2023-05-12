e05886884960b8f94c1e06ac3b552bf1be9b1c24
feitosinha97 <89053915+feitosinha97@users.noreply.github.com>
2023-05-11 22:14:47 -0300

    Adicionando formulários de login e cadastro com rotas

---
+++ front/checkit/src/pages/register.jsx
@@ -0,0 +1,11 @@
+import { RegisterForm } from "../Components/RegisterForm";
+
+function Register() {
+    return (
+      <div>
+        <RegisterForm />
+      </div>
+    );
+  }
+  
+export default Register;
