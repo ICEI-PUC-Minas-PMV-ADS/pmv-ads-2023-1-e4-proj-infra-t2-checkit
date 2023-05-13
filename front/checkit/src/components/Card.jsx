1c2369f826498f185fc82bf2295645095bcf4290
Julia Sartori <juliasartori@outlook.com.br>
2023-05-13 13:39:53 +0200

    grid ok

--- front/checkit/src/components/Card.jsx
+++ front/checkit/src/components/Card.jsx
@@ -1,17 +1,16 @@
 import { Card } from 'primereact/card';
 import TaskItem from './TaskItem'
+import '../style/index.css'
 
 export default function ProjectCard() {
   return (
-    <div className="container">
-        <Card className="mb-5">
-        <h3> titulo aqui fia</h3>
-         <p >
+      <Card className="shadow-sm border border-light project-card m-1">
+        <h3 className="pb-3"> titulo aqui fia</h3>
+         <p className="px-3">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae
         numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!
          </p>
         < TaskItem />
       </Card>
-    </div>
   );
 }
