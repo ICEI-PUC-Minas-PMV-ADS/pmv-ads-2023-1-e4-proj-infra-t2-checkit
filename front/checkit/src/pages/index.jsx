330dbf8d265656b6c9310fd87a89a04f3b83444e
Julia Sartori <juliasartori@outlook.com.br>
2023-05-14 00:56:03 +0200

    getall implemented

--- front/checkit/src/pages/index.jsx
+++ front/checkit/src/pages/index.jsx
@@ -1,12 +1,9 @@
 import ProjectCard from '../components/Card.jsx'
-// import ProjectList from '../components/ProjectList.jsx';
-
 
 export default function IndexGrid() {
   return (
   <div className="container p-3">
   <div className="d-flex flex-wrap text-center">
-      {/* <ProjectList /> */}
       <ProjectCard />
       <ProjectCard />
       <ProjectCard />
--- front/checkit/src/services/Api.js
+++ front/checkit/src/services/Api.js
@@ -1,16 +1,20 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5278/api',
+  baseURL: 'https://localhost:5278/api',
   headers: {
     'Content-Type': 'application/json'
   }
 });
 
+export const getAllProjects = async () => {
+ const response = await api.get("/projects");
+return response.data
+};
+
 export const login = async (email, password) => {
   console.log(email, password);
   const response = await api.post("/users/authenticate", { email, password });
-  //console.log(response.data);
   return response.data;
 };
 
