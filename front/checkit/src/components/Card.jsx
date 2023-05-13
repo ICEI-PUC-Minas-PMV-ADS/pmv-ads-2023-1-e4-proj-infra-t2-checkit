467a1888c4b171d7209a1511b817fb79196d3796
Julia Sartori <juliasartori@outlook.com.br>
2023-05-13 13:47:24 +0200

    all elements from card

--- front/checkit/src/components/Card.jsx
+++ front/checkit/src/components/Card.jsx
@@ -1,15 +1,22 @@
 import { Card } from 'primereact/card';
 import TaskItem from './TaskItem'
+import { ProgressBar } from 'primereact/progressbar';
 import '../style/index.css'
 
 export default function ProjectCard() {
+  const completedTasks = 40
+
   return (
+
+
       <Card className="shadow-sm border border-light project-card m-1">
-        <h3 className="pb-3"> titulo aqui fia</h3>
-         <p className="px-3">
+        <h6 className="h6">prazo em 2 dias</h6>
+        <h3 className="p-3"> titulo aqui fia</h3>
+         <p className="px-3 text-justify">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae
         numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!
          </p>
+         <ProgressBar value={completedTasks}></ProgressBar>
         < TaskItem />
       </Card>
   );
