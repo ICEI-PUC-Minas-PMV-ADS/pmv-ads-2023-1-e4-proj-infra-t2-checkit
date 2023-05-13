1b545fbcf69da853598345bbc27b68e1600834da
Julia Sartori <juliasartori@outlook.com.br>
2023-05-13 13:09:56 +0200

    grid fixed

--- front/checkit/src/components/Card.jsx
+++ front/checkit/src/components/Card.jsx
@@ -3,8 +3,9 @@ import TaskItem from './TaskItem'
 
 export default function ProjectCard() {
   return (
-    <div>
-        <Card className="border border-primary-subtle">
+    <div className="container">
+        <Card className="mb-5">
+        <h3> titulo aqui fia</h3>
          <p >
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae
         numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!
--- front/checkit/src/pages/index.jsx
+++ front/checkit/src/pages/index.jsx
@@ -1,11 +1,30 @@
 import ProjectCard from '../components/Card.jsx'
 
-export default function AutoGrid() {
+export default function IndexGrid() {
   return (
-    <div>
-    < ProjectCard className="m-8"/>
-    < ProjectCard />
-    < ProjectCard />
+  <div className="container pt-5 text-center">
+    <div className="row">
+      <div className="col">
+      < ProjectCard />
+      </div>
+      <div className="col">
+      < ProjectCard />
+      </div>
+      <div className="col">
+      < ProjectCard />
+      </div>
     </div>
+    <div className="row">
+      <div className="col">
+      < ProjectCard />
+      </div>
+      <div className="col">
+      < ProjectCard />
+      </div>
+      <div className="col">
+      < ProjectCard />
+      </div>
+    </div>
+  </div>
   );
 }
