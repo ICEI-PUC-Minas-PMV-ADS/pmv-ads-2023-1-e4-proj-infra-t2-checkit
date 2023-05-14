07305688c162640b057bfae4b0940368705c934b
Julia Sartori <juliasartori@outlook.com.br>
2023-05-14 19:47:30 +0200

    titulo adicionado a index

--- front/checkit/src/pages/index.jsx
+++ front/checkit/src/pages/index.jsx
@@ -13,6 +13,7 @@ export default function IndexGrid() {
   return (
     <>
     <MenuBar />
+    <h1 className="p-5 mx-5 mt-5">Olá! Estes são seus projetos em andamento:</h1>
     <div className="container p-3">
       <div className="d-flex flex-wrap text-center">
       {projects.map(project => <ProjectCard project={project} key={project.key} />)}
