da1099d47b2ba79aa065a82c6e3db0c13fced57d
Gabriel Lopes <gabrielpuneco@gmail.com>
2023-05-14 16:02:13 -0300

    [feature] Implementando Serviço de Busca de CEP no cadastro

--- MobileApp/src/Telas/Usuario/CadastroUsuario.js
+++ MobileApp/src/Telas/Usuario/CadastroUsuario.js
@@ -1,17 +1,60 @@
-import { useState } from "react";
+import axios from "axios";
+import { useState,useEffect } from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { TextInput } from "react-native-paper";
 
 export default function CadastroUsuario() {
   const [numero, setNumero] = useState(0);
+  const[bairro,setBairro]=useState('')
+  const [cidade,setCidade]=useState()
+  const[rua,setRua] = useState()
+  const[uf,setUf]= useState()
+  const [cep,setCep]= useState("")
+  // API: Buscar o Cep
+  useEffect(() => {
+    buscarEndereco();
+  }, [cep]);
 
-  const add = () => {
-    setNumero(numero + 1);
+  
+  const buscarEndereco = () => {
+    console.log(cep)
+    const Cep = (e) => {
+      if (String(cep).length == 8) {
+        const meuCep = String(cep);
+
+        const value = meuCep.replace(/[^0-9]+/, meuCep);
+        const url = `https://viacep.com.br/ws/${value}/json/`;
+
+        fetch(url)
+          .then((response) => response.json())
+          .then((json) => {
+            if (json.logradouro) {
+              setBairro(json.bairro);
+              setCidade(json.localidade);
+              setRua(json.logradouro);
+              setUf(json.uf)
+            }
+          });
+      }
+    };
+    Cep();
   };
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <TouchableOpacity onPressOut={add}>
+      <TouchableOpacity >
         <Text>{numero} asdasd </Text>
+
+        <TextInput value={cep} onChangeText={setCep}/>
+        <TextInput value={cidade}/>
+        <TextInput value={bairro}/>
+        <TextInput value={uf}/>
+        <TextInput value={rua}/>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={buscarEndereco}>
+        
+        <Text>asfasdfas</Text>
+
       </TouchableOpacity>
     </View>
   );
