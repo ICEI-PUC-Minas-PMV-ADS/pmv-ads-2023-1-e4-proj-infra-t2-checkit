b0183c1563a2fa2b94406df6106dc22b53bad7c1
Julia Sartori <juliasartori@outlook.com.br>
2023-05-14 13:20:47 +0200

    style on login form

--- front/checkit/src/pages/login.jsx
+++ front/checkit/src/pages/login.jsx
@@ -1,17 +1,12 @@
 import { LoginForm } from "../components/LoginForm";
 import '../style/login.css'
-import { Card } from 'primereact/card'
-
 
 
 function Login() {
     return (
-      <div className="container p-3">
-        <Card className="shadow-sm border border-light rounded project-card m-1">
+        <div className="p-5">
         <LoginForm />
-        </Card>
-
-      </div>
+        </div >
     );
   }
 
