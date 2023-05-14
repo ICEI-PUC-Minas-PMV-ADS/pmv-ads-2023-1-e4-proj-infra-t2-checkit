817566c6003cc4b9a9c18d1273cff1b5d81e3af9
Julia Sartori <juliasartori@outlook.com.br>
2023-05-14 13:40:46 +0200

    css form registro

--- front/checkit/src/components/RegisterForm.jsx
+++ front/checkit/src/components/RegisterForm.jsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
 import { register } from "../services/Api";
+import { InputText } from "primereact/inputtext";
+import { Button } from 'primereact/button';
+import { useNavigate } from "react-router-dom";
+
 
 export function RegisterForm() {
   const [email, setEmail] = useState("");
   const [pass, setPass] = useState("");
   const [name, setName] = useState("");
+
+  const navigate = useNavigate();
+
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -14,42 +20,47 @@ export function RegisterForm() {
     console.log(data);
   };
 
+  const handleClick = () => {
+    navigate("/");
+  };
+
     return (
-        <>
-            <form onSubmit={handleSubmit} className="new-item-form">
-                <div className="form-row">
-                    <h2>Register</h2>
-                    <label htmlFor="name">Full name</label>
-                    <input
-                        value={name}
-                        name="name"
-                        onChange={(e) => setName(e.target.value)}
-                        id="name-item"
-                        placeholder="full Name" />
-                    <label htmlFor="item">Email</label>
-                    <input
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        type="email"
-                        placeholder="youremail@gmail.com"
-                        id="email-item"
-                        name="email"
-                    />
-                    <label htmlFor="item">Password</label>
-                    <input
-                        value={pass}
-                        onChange={(e) => setPass(e.target.value)}
-                        type="password"
-                        placeholder="**************"
-                        id="pass- item"
-                        name="password"
-                    />
-                </div>
-                <button className="btn" type="submit">Register</button>
-            </form>
-            <Link to="/">
-                <button className="btn">Already have an account? Login here.</button>
-            </Link>
-        </>
+      <div className="form-container d-flex justify-content-center text-center">
+            <form onSubmit={handleSubmit} className="p-5 mx-5">
+              <h2>Criar conta</h2>
+              <div className="p-3">
+                <span className="p-float-label">
+                  <InputText id="name" value={name} onChange={(e) => setName(e.target.value)} placeholder="Seu nome completo"/>
+                  <label className="px-2" htmlFor="name">Nome</label>
+                </span>
+              </div>
+             <div className="p-3">
+              <span className="p-float-label">
+                <InputText id="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="youremail@gmail.com"/>
+                <label className="px-2" htmlFor="email">Email</label>
+              </span>
+            </div>
+            <div className="p-3">
+            <span className="p-float-label">
+              <InputText
+                value={pass}
+                onChange={(e) => setPass(e.target.value)}
+                placeholder="**************"
+                id="senha"
+              />
+              <label className="px-2" htmlFor="senha">Senha</label>
+            </span>
+          </div>
+          <div className="p-3">
+            <Button  className="btn" id="register" label="Criar conta" type="submit"/>
+          </div>
+          <label htmlFor="register" className="px-3 mt-3 mb-0 text-muted">
+            Já é cadastrado?
+          </label>
+          <div className="px-3">
+  `        <Button className="btn" label="Login" onClick={handleClick}/>
+          </div>
+        </form>
+    </div>
     )
 }
