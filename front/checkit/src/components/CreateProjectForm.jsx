0e9e011b8b984494e9fe8011257802ddeda35054
Julia Sartori <juliasartori@outlook.com.br>
2023-05-16 15:42:24 +0200

    basic createForm

---
+++ front/checkit/src/components/CreateProjectForm.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { InputText } from "primereact/inputtext";
+import { Button } from 'primereact/button';
+import { Calendar } from 'primereact/calendar';
+
+export function CreateProjectForm() {
+  const navigate = useNavigate();
+
+  const [titulo, setTitulo] = useState(null);
+  const [prazo, setPrazo] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(e);
+    navigate("/");
+  };
+
+<form onSubmit={handleSubmit} className="p-5 mx-5 form-container">
+          <h2>Novo projeto</h2>
+          <div className="p-3">
+            <span className="p-float-label">
+              <InputText id="nome" value={titulo} placeholder="Título do seu projeto"/>
+              <label className="px-2" htmlFor="titulo">Título</label>
+            </span>
+          </div>
+          <div className="p-3">
+            <span className="p-float-label">
+              <Calendar value={prazo} onChange={(e) => setDate(e.value)} />
+              <label className="px-2" htmlFor="Prazo">Prazo</label>
+            </span>
+          </div>
+          <div className="justify-content-around px-3">
+          <Button className="btn cyan-50" label="Criar novo projeto" type="submit"/>
+          </div>
+
+      </form>
+
+}
