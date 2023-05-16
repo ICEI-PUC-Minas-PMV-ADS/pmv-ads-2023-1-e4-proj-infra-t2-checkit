18cd412b10ee599e77b4190d8cf0341e1693ff46
Julia Sartori <juliasartori@outlook.com.br>
2023-05-17 00:07:29 +0200

    user form dialog

--- front/checkit/src/components/Header.jsx
+++ front/checkit/src/components/Header.jsx
@@ -2,6 +2,7 @@ import { Menubar } from 'primereact/menubar';
 import { useState } from 'react';
 import { BsGrid1X2Fill, BsPlusSquareFill, BsGearFill, BsDoorClosedFill } from "react-icons/bs"
 import CreateProject from './CreateProject';
+import EditUserForm from './EditUserForm';
 import { Dialog } from 'primereact/dialog';
 
 
@@ -9,6 +10,8 @@ import { Dialog } from 'primereact/dialog';
 export default function MenuBar() {
 
   const [displayCreateForm, setDisplayCreateForm] = useState(false);
+  const [displayUserForm, setDisplayUserForm] = useState(false);
+
 
   const items = [
         {
@@ -21,6 +24,7 @@ export default function MenuBar() {
         },
         {
             icon: <BsGearFill />,
+            command: () => setDisplayUserForm(true)
         }
   ]
   const end = < BsDoorClosedFill className="mx-3" />;
@@ -29,11 +33,18 @@ export default function MenuBar() {
       setDisplayCreateForm(false);
     };
 
+    const onHideUser = () => {
+      setDisplayUserForm(false);
+    };
+
   return (
         <div className="card">
           <Menubar model={items} end={end} />
           <Dialog visible={displayCreateForm} onHide={onHide} header="Novo projeto">
             <CreateProject />
+          </Dialog>
+          <Dialog visible={displayUserForm} onHide={onHideUser} header="Editar usuário">
+            <EditUserForm />
           </Dialog>
         </div>
     )
