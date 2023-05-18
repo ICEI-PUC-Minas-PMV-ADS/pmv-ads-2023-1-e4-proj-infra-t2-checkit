cc754fbec4fe1e0346fe557fb2e80da59752a708
Ana Paula Buchholz <anapbuchholz@gmail.com>
2023-05-18 19:41:10 -0300

    [feature] Página de cadastro de usuário e login

--- MobileApp/src/Telas/Usuario/CadastroUsuario.js
+++ MobileApp/src/Telas/Usuario/CadastroUsuario.js
@@ -1,30 +1,139 @@
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+const App = () => {
+  const onPressLogin = async () => {
+    // Do something about login operation
+    console.log("funciona");
 
-import { useState } from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-
-export default function CadastroUsuario() {
-  const [numero, setNumero] = useState(0)
-
-  const add = () => {
-    setNumero(numero + 1)
-
-
-  }
+    fetch(
+      "https://api.nasa.gov/planetary/apod?api_key=gkimsne4yrAAj6jBFaTrAIUn9DxWkRlq4ZGDWqen"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        // Do something with the data
+      })
+      .catch((error) => {
+        console.log(error);
+        // Handle any errors
+      });
+  };
+  const onPressForgotPassword = () => {
+    // Do something about forgot password operation
+  };
+  const onPressSignUp = () => {
+    // Do something about signup operation
+  };
+  const [state, setState] = useState({
+    email: "",
+    password: "",
+  });
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <TouchableOpacity onPressOut={add}><Text>{numero
-      } asdasd </Text></TouchableOpacity>
-
+      <Text style={styles.title}>Insira seus dados para iniciar um projeto com CheckIt!</Text>
+      <View style={styles.inputView}>
+      <Text style={styles.infoInputText}>Nome</Text>
+      <TextInput
+          style={styles.inputText}
+          placeholderTextColor="#003f5c"
+          onChangeText={(text) => setState({ nome: text })}
+        />
+         <Text style={styles.infoInputText}>E-mail</Text>
+        <TextInput
+          style={styles.inputText}
+          placeholderTextColor="#003f5c"
+          onChangeText={(text) => setState({ email: text })}
+        />
+        <Text style={styles.infoInputText} >Senha</Text>
+        <TextInput
+          style={styles.inputText}
+          secureTextEntry
+          placeholderTextColor="#003f5c"
+          onChangeText={(text) => setState({ password: text })}
+        />
+        <Text style={styles.infoInputText}>Confirmar senha</Text>
+        <TextInput
+          style={styles.inputText}
+          secureTextEntry
+          placeholderTextColor="#003f5c"
+          onChangeText={(text) => setState({ password: text })}
+        />
+      </View>
+      <TouchableOpacity onPress={onPressLogin} style={styles.loginBtn}>
+        <Text style={styles.loginText}>Estou pronto </Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={onPressSignUp}>
+        <Text style={styles.signUpText}>Já tem uma conta? Login!</Text>
+      </TouchableOpacity>
     </View>
   );
-}
-
+};
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#ffffff",
+    alignItems: "center",
+    justifyContent: "space-between",
   },
+  title: {
+    height: 200,
+    fontWeight: "bold",
+    fontSize: 25,
+    color: "#000000",
+    marginTop: 85,
+    padding: 30
+  },
+  inputView: {
+    width: "80%",
+    borderColor: "black",
+    backgroundColor: "#ffffff",
+    borderRadius: 11,
+    height: 50,
+    marginBottom: 2,
+    justifyContent: "center",
+    display: "flex"
+  },
+  inputText: {
+    height: 50,
+    marginBottom: 20,
+    color: "white",
+    borderColor: '#000000',
+    borderWidth: 1,
+    borderRadius: 5,
+    display: "flex"
+  },
+  signUpText: {
+    bottom: 70,
+    color: "#505050",
+    fontSize: 11,
+  },
+  forgotText: {
+    color: "#505050",
+    fontSize: 11,
+  },
+  loginBtn: {
+    width: "80%",
+    color: "#85B1E4",
+    backgroundColor: "#5C66BD",
+    borderRadius: 11,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    marginBottom: 10,
+    display: "flex"
+  },
+  loginText: {
+    color: "#ffffff",
+  },
+  infoInputText: {
+    fontSize: 12
+  }
 });
+export default App;
