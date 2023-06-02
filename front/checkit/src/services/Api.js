23ae61f5d2c431b019e22d03a3d2239e7826a685
Julia Sartori <juliasartori@outlook.com.br>
2023-06-02 18:54:12 +0200

    gets projects and tasks by id

--- front/checkit/src/services/Api.js
+++ front/checkit/src/services/Api.js
@@ -1,27 +1,31 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://localhost:5278/api',
   headers: {
     'Content-Type': 'application/json'
   }
 });
 
 export const getAllProjects = async () => {
- const response = await api.get("/projects");
+ const response = await api.get("https://localhost:5278/api/projects");
 return response.data
 };
 
 export const login = async (email, password) => {
   console.log(email, password);
-  const response = await api.post("/users/authenticate", { email, password });
+  const response = await api.post("https://localhost:5278/api/users/authenticate", { email, password });
   //console.log(response.data);
   return response.data;
 };
 
 
 export const register = async (name, email, password, role = 1) => {
-  const response = await api.post("/users", { name, email, password, role });
+  const response = await api.post("https://localhost:5278/api/users", { name, email, password, role });
+  return response.data;
+};
+
+export const getTasksByProject = async (projectId) => {
+  const response = await axios.get(`https://localhost:7152/api/Projects/GetTaskFromProject/${projectId}`);
   return response.data;
 };
 
