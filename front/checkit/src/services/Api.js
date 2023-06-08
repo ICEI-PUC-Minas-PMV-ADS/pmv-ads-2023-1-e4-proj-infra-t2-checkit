a0019bac2af98baf7cedd7f8ce7b9c88dd6df288
Julia Sartori <juliasartori@outlook.com.br>
2023-06-08 17:33:41 +0200

    api ok

--- front/checkit/src/services/Api.js
+++ front/checkit/src/services/Api.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import validateToken from "./ValidateToken";
 
 const api = axios.create({
   headers: {
@@ -18,6 +19,7 @@ return response.data
 export const login = async (email, password) => {
   console.log(email, password);
   const response = await api.post("https://localhost:5278/api/users/authenticate", { email, password });
+  localStorage.setItem("jwtToken", JSON.stringify(response.data));
   //console.log(response.data);
   return response.data;
 };
@@ -30,6 +32,23 @@ export const register = async (name, email, password, role = 1) => {
 
 export const getTasksByProject = async (projectId) => {
   const response = await axios.get(`https://localhost:7152/api/Projects/GetTaskFromProject/${projectId}`);
+  return response.data;
+};
+
+
+export const updateUser = async (name, password, email) => {
+  // Fetch user data to get the ID
+  const userDataResponse = await api.get("https://localhost:5278/api/users");
+  const user = userDataResponse.data.find((user) => user.email === email);
+
+  if (!user) {
+    throw new Error("User not found.");
+  }
+
+  const id = user.id;
+
+  // Update user data
+  const response = await api.put(`/users/${id}`, { name, email, password });
   return response.data;
 };
 
