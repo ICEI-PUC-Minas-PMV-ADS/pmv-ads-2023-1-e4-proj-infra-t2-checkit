9ffc559bcdb341b6c905df34e5bc7dcb059bdbf6
Carlos Camuzzi <carloscamuzzi.hs@gmail.com>
2023-06-08 19:12:24 -0300

    ajustes layout login e cadastro usuario

--- MobileApp/src/Telas/Usuario/CadastroUsuario.js
+++ MobileApp/src/Telas/Usuario/CadastroUsuario.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View, TouchableOpacity, Alert } from "react-native";
 import Input from "../../Componentes/Input";
 
 import Body from "../../Componentes/Body";
-import { Button,Snackbar } from "react-native-paper";
+import { Button, Snackbar } from "react-native-paper";
 import { useNavigation } from "@react-navigation/native";
 import { TextInput } from "react-native-paper";
 const CadastroUsuario = () => {
@@ -15,7 +15,6 @@ const CadastroUsuario = () => {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [aviso, setAviso] = useState("");
-
 
   const [visible, setVisible] = useState(false);
   const onToggleSnackBar = () => setVisible(!visible);
@@ -40,19 +39,17 @@ const CadastroUsuario = () => {
   //     });
   // };
 
-
   const handleRegister = () => {
     //console.log(`${name}, ${email}, ${password}, ${confirmPassword}`);
     // Validando senha
     if (!name || !email || !password || !confirmPassword) {
       console.log("missinfo");
       setMissInfo(true);
-      onToggleSnackBar()
-      setAviso("Preencha todos os campos para realizarmos o seu cadastro")
+      onToggleSnackBar();
+      setAviso("Preencha todos os campos para realizarmos o seu cadastro");
     }
     if (password !== confirmPassword) {
-      setAviso("Insira senhas iguais")
-
+      setAviso("Insira senhas iguais");
     } else {
       console.log("Ir para tela inicial");
     }
@@ -61,102 +58,94 @@ const CadastroUsuario = () => {
   return (
     <View style={styles.container}>
       <Body>
-
-        <View style={{width:300}}>
+        <View>
           <Text style={styles.title}>
-          Insira seus dados para iniciar um projeto com CheckIt!
+            Insira seus dados para iniciar um projeto com CheckIt!
           </Text>
         </View>
-
-        <Text style={styles.infoInputText}>Nome</Text>
-        <Input
-         mode="outlined"
-         outlineColor={"#262626"}
-         activeOutlineColor={"#262626"}
-          placeholderTextColor="#003f5c"
-          onChangeText={(name) => setName(name)}
-          right={<TextInput.Icon icon="account-circle-outline" />}
-
-        />
-        <Text style={styles.infoInputText}>E-mail</Text>
-        <Input
-         mode="outlined"
-         outlineColor={"#262626"}
-         activeOutlineColor={"#262626"}
-
-          placeholderTextColor="#003f5c"
-          onChangeText={(email) => setEmail(email)}
-          right={<TextInput.Icon icon="email-outline" />}
-        />
-        <Text style={styles.infoInputText}>Senha</Text>
-        <Input
+        <View style={{ marginHorizontal: 25 }}>
+          <Text style={styles.infoInputText}>Nome</Text>
+          <Input
             mode="outlined"
-         activeOutlineColor={"#262626"}
-
             outlineColor={"#262626"}
-          secureTextEntry={escondeSenha}
-          placeholderTextColor="#003f5c"
-          right={
-            <TextInput.Icon
-              onPress={() =>
-                escondeSenha ? setEscondeSenha(false) : setEscondeSenha(true)
-              }
-              icon={escondeSenha ? "eye-off" : "eye"}
-            />
-          }
-          onChangeText={(password) => setPassword(password)}
-        />
-        <Text style={styles.infoInputText}>Confirmar senha</Text>
-        <Input
-         mode="outlined"
-         outlineColor={"#262626"}
-         activeOutlineColor={"#262626"}
-
-          secureTextEntry={escondeConfirmaSenha}
-          placeholderTextColor="#003f5c"
-          right={
-            <TextInput.Icon
-              onPress={() =>
-                escondeConfirmaSenha ? setEscondeConfirmaSenha(false) : setEscondeConfirmaSenha(true)
-              }
-              icon={escondeConfirmaSenha ? "eye-off" : "eye"}
-            />
-          }
-          onChangeText={(confirmPassword) => {
-            setConfirmPassword(confirmPassword);
-          }}
-        />
-        {/* <TouchableOpacity onPress={onPressLogin} style={styles.loginBtn}>
+            activeOutlineColor={"#262626"}
+            placeholderTextColor="#003f5c"
+            onChangeText={(name) => setName(name)}
+            right={<TextInput.Icon icon="account-circle-outline" />}
+          />
+          <Text style={styles.infoInputText}>E-mail</Text>
+          <Input
+            mode="outlined"
+            outlineColor={"#262626"}
+            activeOutlineColor={"#262626"}
+            placeholderTextColor="#003f5c"
+            onChangeText={(email) => setEmail(email)}
+            right={<TextInput.Icon icon="email-outline" />}
+          />
+          <Text style={styles.infoInputText}>Senha</Text>
+          <Input
+            mode="outlined"
+            activeOutlineColor={"#262626"}
+            outlineColor={"#262626"}
+            secureTextEntry={escondeSenha}
+            placeholderTextColor="#003f5c"
+            right={
+              <TextInput.Icon
+                onPress={() =>
+                  escondeSenha ? setEscondeSenha(false) : setEscondeSenha(true)
+                }
+                icon={escondeSenha ? "eye-off" : "eye"}
+              />
+            }
+            onChangeText={(password) => setPassword(password)}
+          />
+          <Text style={styles.infoInputText}>Confirmar senha</Text>
+          <Input
+            mode="outlined"
+            outlineColor={"#262626"}
+            activeOutlineColor={"#262626"}
+            secureTextEntry={escondeConfirmaSenha}
+            placeholderTextColor="#003f5c"
+            right={
+              <TextInput.Icon
+                onPress={() =>
+                  escondeConfirmaSenha
+                    ? setEscondeConfirmaSenha(false)
+                    : setEscondeConfirmaSenha(true)
+                }
+                icon={escondeConfirmaSenha ? "eye-off" : "eye"}
+              />
+            }
+            onChangeText={(confirmPassword) => {
+              setConfirmPassword(confirmPassword);
+            }}
+          />
+          {/* <TouchableOpacity onPress={onPressLogin} style={styles.loginBtn}>
             <Text style={styles.loginText}>Estou pronto </Text>
           </TouchableOpacity> */}
-        <View style={styles.viewBtn}>
-        
-        <TouchableOpacity onPress={handleRegister} style={styles.loginBtn} >
-          
-            <Text style={styles.textBtn}>Estou pronto</Text>
-   
-        </TouchableOpacity>
-        </View>
-        <View style={styles.viewOr}>
-        
-        
-          <Text>OU</Text>
-       
-        </View>
-        <View style={styles.viewText}>
+          <View style={styles.viewBtn}>
+            <TouchableOpacity onPress={handleRegister} style={styles.loginBtn}>
+              <Text style={styles.textBtn}>Estou pronto</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.viewOr}>
+            <Text style={{ color: "#FFF" }}>OU</Text>
+          </View>
+          <View style={styles.viewText}>
+            <TouchableOpacity onPress={() => navigation.navigate("Login")}>
+              <Text style={styles.signUpText}>Já tem uma conta? Login!</Text>
+            </TouchableOpacity>
+          </View>
 
-        <TouchableOpacity onPress={() => navigation.navigate("Login")}>
-          <Text style={styles.signUpText}>Já tem uma conta? Login!</Text>
-        </TouchableOpacity>
-        <Snackbar
-          visible={visible}
-          onDismiss={onDismissSnackBar}
-          action={{
-            label: "Ok",
-          }}
-        >
-         {aviso}
-        </Snackbar>
+          <Snackbar
+            visible={visible}
+            onDismiss={onDismissSnackBar}
+            action={{
+              label: "Ok",
+            }}
+          >
+            {aviso}
+          </Snackbar>
         </View>
       </Body>
     </View>
@@ -168,8 +157,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#ffffff",
     alignItems: "center",
     backgroundColor: "#7D8AFF",
-
-
   },
   title: {
     height: 140,
@@ -177,12 +164,12 @@ const styles = StyleSheet.create({
     fontSize: 24,
     color: "#000000",
     marginTop: 10,
-    marginBottom:5,
-  
+    marginBottom: 5,
+    textAlign: "center",
     padding: 5,
-    color:"white",
-    letterSpacing:6.7,
-    width:380
+    color: "white",
+    letterSpacing: 6.2,
+    width: 380,
   },
   inputView: {
     width: "80%",
@@ -205,10 +192,8 @@ const styles = StyleSheet.create({
   signUpText: {
     bottom: 70,
     color: "white",
-    fontWeight:"bold",
+    fontWeight: "bold",
     fontSize: 12,
-    
-
   },
   forgotText: {
     color: "#505050",
@@ -224,7 +209,6 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     marginTop: 20,
     marginBottom: 10,
-
   },
   loginText: {
     color: "#ffffff",
@@ -232,32 +216,31 @@ const styles = StyleSheet.create({
     paddingTop: 10,
   },
   viewBtn: {
-    
     justifyContent: "center",
     alignItems: "center",
     alignSelf: "center",
   },
-  viewOr:{
+  viewOr: {
     justifyContent: "center",
     alignItems: "center",
     alignSelf: "center",
-    marginTop:10,
-    marginBottom:-10,
+    marginTop: 10,
+    marginBottom: -10,
   },
-  viewText:{
+  viewText: {
     width: 320,
     justifyContent: "center",
     alignItems: "center",
     marginTop: 100,
   },
-  textBtn:{
-    fontSize:16,
-    color:'white'
+  textBtn: {
+    fontSize: 16,
+    color: "white",
   },
   infoInputText: {
     fontSize: 14,
     fontWeight: "bold",
-    color:"white",
+    color: "white",
     margin: 8,
   },
 });
--- MobileApp/src/Telas/Usuario/Login.js
+++ MobileApp/src/Telas/Usuario/Login.js
@@ -1,12 +1,14 @@
 import React, { useContext, useState } from "react";
 import API from "../../Services/webapiservices";
-import { StyleSheet, Text, View, TouchableOpacity,Image } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { TextInput, Snackbar } from "react-native-paper";
 import { AuthUserContext } from "../../Contexts/AuthUserProvider";
+import Container from "../../Componentes/Container";
+import Body from "../../Componentes/Body";
 const App = () => {
   const navigation = useNavigation();
-  const {postLogin,user,setUser} = useContext(AuthUserContext)
+  const { postLogin, user, setUser } = useContext(AuthUserContext);
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -17,147 +19,135 @@ const App = () => {
 
   const onToggleSnackBar = () => setVisible(!visible);
   const onDismissSnackBar = () => setVisible(false);
- 
+
   const onPressLogin = () => {
-    
     console.log("funciona");
 
     if (!email && !password) {
       setMissInfo(true); // Ausência de email e/ou senha
       onToggleSnackBar();
-      setAviso("Por favor insira o seu Email e Senha")
+      setAviso("Por favor insira o seu Email e Senha");
       // setAviso("Por favor, insira o email e a senha");
-    } else if(email.length==0) {
+    } else if (email.length == 0) {
       onToggleSnackBar();
-      setAviso("Por favor insira o seu Email")
-    
-    } 
-    else if(password.length==0){
+      setAviso("Por favor insira o seu Email");
+    } else if (password.length == 0) {
       onToggleSnackBar();
-      setAviso("Por favor insira a sua Senha")
+      setAviso("Por favor insira a sua Senha");
     }
-  };
-  const onPressForgotPassword = () => {
-    // Do something about forgot password operation
-  };
-  const onPressSignUp = () => {
-    // Do something about signup operation
   };
 
   return (
     <View style={styles.container}>
-      
-      <View style={styles.viewLogo}>
-
-      <Image
-          style={styles.logo}
-          source={require("../../../assets/Logos/logo_transparent.png")}
-        />
-      </View>
-     
-      
-      
-      <View style={styles.inputView}>
-
-        <Text style={styles.infoInputText}>E-mail</Text>
-        <TextInput
-           mode="outlined"
-           outlineColor="#262626"
-           activeOutlineColor="#262626"
-          
-          style={styles.inputText}
-          value={email}
-          right={<TextInput.Icon icon="email-outline" />}
-          placeholderTextColor="#003f5c"
-          error={missInfo || (missInfo && !email) ? true : false}
-          onChangeText={(text) => setEmail(text)}
-        />
-      
-
-        <Text style={styles.infoInputText}>Senha</Text>
-        <TextInput
-          mode="outlined"
-          outlineColor={"#262626"}
-          style={styles.inputText}
-          activeOutlineColor="#262626"
-          error={missInfo || (missInfo && !password) ? true : false}
-          secureTextEntry={escondeSenha}
-          right={
-            <TextInput.Icon
-              onPress={() =>
-                escondeSenha ? setEscondeSenha(false) : setEscondeSenha(true)
-              }
-              icon={escondeSenha ? "eye-off" : "eye"}
+      <Body>
+        <View style={styles.viewLogo}>
+          <Image
+            style={styles.logo}
+            source={require("../../../assets/Logos/logo_transparent.png")}
+          />
+          <View style={{ marginHorizontal: 25 }}>
+            <Text style={styles.infoInputText}>E-mail</Text>
+            <TextInput
+              mode="outlined"
+              outlineColor="#262626"
+              activeOutlineColor="#262626"
+              style={styles.inputText}
+              value={email}
+              right={<TextInput.Icon icon="email-outline" />}
+              placeholderTextColor="#003f5c"
+              error={missInfo || (missInfo && !email) ? true : false}
+              onChangeText={(text) => setEmail(text)}
             />
-          }
-          placeholderTextColor="#003f5c"
-          value={password}
-          onChangeText={(text) => setPassword(text)}
-        />
-      </View>
-      {missInfo && <Text style={styles.aviso}>{aviso}</Text>}
-      <View style={styles.viewButton}>
 
-      <TouchableOpacity onPress={onPressLogin} style={styles.loginBtn}>
-        <Text style={styles.loginText}>Entrar </Text>
-      </TouchableOpacity>
-      </View>
-      <View style={styles.viewAvisoCadastro}>
-      <TouchableOpacity onPress={() => navigation.navigate("CadastroUsuario")}>
-        <Text style={styles.signUpText}>
-          Ainda não tem uma conta? Cadastre-se!
-        </Text>
-      </TouchableOpacity>
-      </View>
-      <Snackbar
+            <Text style={styles.infoInputText}>Senha</Text>
+            <TextInput
+              mode="outlined"
+              outlineColor={"#262626"}
+              style={styles.inputText}
+              activeOutlineColor="#262626"
+              error={missInfo || (missInfo && !password) ? true : false}
+              secureTextEntry={escondeSenha}
+              right={
+                <TextInput.Icon
+                  onPress={() =>
+                    escondeSenha
+                      ? setEscondeSenha(false)
+                      : setEscondeSenha(true)
+                  }
+                  icon={escondeSenha ? "eye-off" : "eye"}
+                />
+              }
+              placeholderTextColor="#003f5c"
+              value={password}
+              onChangeText={(text) => setPassword(text)}
+            />
+          </View>
+        </View>
+
+        <View style={{ alignItems: "center" }}>
+          {missInfo && <Text style={styles.aviso}>{aviso}</Text>}
+          <View style={styles.viewButton}>
+            <TouchableOpacity onPress={onPressLogin} style={styles.loginBtn}>
+              <Text style={styles.loginText}>Entrar </Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.viewAvisoCadastro}>
+            <TouchableOpacity
+              onPress={() => navigation.navigate("CadastroUsuario")}
+            >
+              <Text style={styles.signUpText}>
+                Ainda não tem uma conta? Cadastre-se!
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        <Snackbar
           visible={visible}
           onDismiss={onDismissSnackBar}
           action={{
             label: "Ok",
           }}
         >
-         {aviso}
+          {aviso}
         </Snackbar>
+      </Body>
     </View>
   );
 };
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: "center",
     backgroundColor: "#7D8AFF",
-    alignItems: "center",
-    justifyContent: "space-between",
   },
-  viewLogo:{
-    marginTop:20,
-
-    marginBottom:-20,
-    marginTop:70,
+  viewLogo: {
+    marginBottom: -20,
+    marginTop: 70,
   },
   logo: {
     height: 190,
     width: 170,
     padding: 10,
     marginBottom: 0,
-    marginTop:0,
+    marginTop: 0,
     alignSelf: "center",
   },
- 
+
   title: {
     height: 200,
-    width:450,
+    width: 450,
     fontWeight: "bold",
     fontSize: 25,
     color: "#ffff",
     marginTop: -200,
     top: 30,
     padding: 50,
-    letterSpacing:5,
-    marginLeft:30,
+    letterSpacing: 5,
+    marginLeft: 30,
   },
   inputView: {
     width: "80%",
-  
     borderRadius: 11,
     height: 50,
     marginBottom: 2,
@@ -168,17 +158,14 @@ const styles = StyleSheet.create({
     height: 35,
     marginBottom: 15,
     color: "black",
-   
-
     borderRadius: 5,
-  
     padding: 4,
   },
   signUpText: {
     marginBottom: 60,
-    alignSelf:"center",
+    alignSelf: "center",
     color: "white",
-    fontWeight:'bold',
+    fontWeight: "bold",
     fontSize: 11,
   },
   forgotText: {
@@ -186,35 +173,35 @@ const styles = StyleSheet.create({
     fontSize: 11,
   },
   loginBtn: {
-    
-    width:200,
+    width: 140,
     color: "#85B1E4",
     backgroundColor: "#FEC044",
     borderRadius: 8,
     height: 50,
-    
-    marginTop: 20,
-    marginBottom: 20,
-    
-  },
-  viewButton:{
-    width:130,
     alignItems: "center",
     justifyContent: "center",
+    marginTop: 20,
+    marginBottom: 10,
+  },
+  viewButton: {
+    width: 130,
+    alignItems: "center",
+    justifyContent: "center",
+    marginVertical: 20,
   },
   loginText: {
     color: "#ffffff",
-    textAlign:"center",
-    alignSelf:"center",
-    justifyContent:"center"
-    
+    textAlign: "center",
+    alignSelf: "center",
+    justifyContent: "center",
+    fontSize: 16,
   },
   infoInputText: {
     fontSize: 14,
-    color:"white",
-    marginBottom:10,
-    marginLeft:3,
-
+    color: "white",
+    marginBottom: 10,
+    marginLeft: 3,
+    fontWeight: "bold",
   },
   aviso: {
     marginTop: 10,
@@ -223,7 +210,6 @@ const styles = StyleSheet.create({
     color: "#D32F2F",
     fontStyle: "italic",
     fontWeight: "bold",
-  
   },
 });
 export default App;
