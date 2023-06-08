321ec09c9603da13def54685bbac0ff81541503c
Julia Sartori <juliasartori@outlook.com.br>
2023-06-08 17:36:04 +0200

    authcontext file added

---
+++ front/checkit/src/services/AuthContext.jsx
@@ -0,0 +1,15 @@
+import React, { createContext, useState } from "react";
+
+export const AuthContext = createContext();
+
+function AuthProvider({ children }) {
+  const [auth, setAuth] = useState(false);
+
+  return (
+    <AuthContext.Provider value={{ auth, setAuth }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export default AuthProvider
