3d39831e9d313633c90a3c0254b2c10369ee8f91
Julia Sartori <juliasartori@outlook.com.br>
2023-06-11 16:45:28 +0200

    temporary modification of routing

--- front/checkit/src/App.jsx
+++ front/checkit/src/App.jsx
@@ -1,13 +1,14 @@
-import {useContext} from 'react';
-import { AuthContext } from './services/AuthContext';
 import 'bootstrap/dist/css/bootstrap.css';
 import "./style/styles.css";
 import '@fontsource/roboto/300.css';
 import 'primeicons/primeicons.css';
 import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.css';
-import PrivateRoutes from './routes/private.routes';
-import PublicRoutes from './routes/public.routes';
+import {useContext} from 'react';
+import { AuthContext } from './services/AuthContext';
+// import PrivateRoutes from './routes/private.routes';
+// import PublicRoutes from './routes/public.routes';
+import AllRoutes from './routes/all.routes';
 
 
 
@@ -15,5 +16,6 @@ import PublicRoutes from './routes/public.routes';
 export default function App() {
   const { auth } = useContext(AuthContext)
   console.log("auth", auth)
-  return auth ? <PrivateRoutes /> : <PublicRoutes />
+  // return auth ? <PrivateRoutes /> : <PublicRoutes />
+  return auth ? <AllRoutes /> : <AllRoutes />
 }
--- front/checkit/src/routes/all.routes.jsx
+++ front/checkit/src/routes/all.routes.jsx
@@ -1,15 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import IndexGrid from '../pages/index.jsx';
+import Login from '../pages/login.jsx';
+import Register from '../pages/Register.jsx';
 
 
-const PrivateRoutes = () => {
+const AllRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/index" element={<IndexGrid />}  />
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
 };
 
-export default PrivateRoutes;
+export default AllRoutes;
