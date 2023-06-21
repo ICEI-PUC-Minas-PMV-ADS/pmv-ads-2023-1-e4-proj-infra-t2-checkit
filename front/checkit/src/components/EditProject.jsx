dc176af9cc762431e53fb109337c7e489b322e7b
54rtor1 <1929sartori@gmail.com>
2023-06-21 20:07:50 +0200

    tarefas and projects on edit form display correctly

--- front/checkit/src/components/EditProject.jsx
+++ front/checkit/src/components/EditProject.jsx
@@ -1,5 +1,37 @@
+import { useState } from "react";
 import ProjectForm from "./ProjectForm";
+import { useProjects } from "../contexts/ProjectsProvider";
 
-export default function EditProjectForm({ project, onSubmit }) {
-  return <ProjectForm project={project} onSubmit={onSubmit} />;
+export default function EditProjectForm({ projectId, project, onSubmit }) {
+  const [editedProject, setEditedProject] = useState(project);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setEditedProject((prevProject) => ({
+      ...prevProject,
+      [name]: value,
+    }));
+  };
+
+  const { updateProject } = useProjects();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await updateProject(projectId, editedProject);
+      onSubmit(editedProject);
+    } catch (error) {
+      console.error(error);
+      // Handle error as needed
+    }
+  };
+
+  return (
+    <ProjectForm
+      project={editedProject}
+      onSubmit={handleSubmit}
+      onChange={handleInputChange}
+    />
+  );
 }
