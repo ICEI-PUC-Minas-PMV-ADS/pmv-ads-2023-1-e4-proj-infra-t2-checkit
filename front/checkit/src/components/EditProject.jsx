7940843e54464abe097147ef9f6f313401c9c250
54rtor1 <1929sartori@gmail.com>
2023-06-22 19:15:24 +0200

    fixture on edit project

--- front/checkit/src/components/EditProject.jsx
+++ front/checkit/src/components/EditProject.jsx
@@ -4,20 +4,14 @@ import { useProjects } from "../contexts/ProjectsProvider";
 export default function EditProjectForm({ projectId, project, onSubmit }) {
   const { updateProject } = useProjects();
 
-  const handleSubmit = async (e) => {
-    if (e) {
-      e.preventDefault();
-    }
+  const handleSubmit = async () => {
     try {
-      // Update the project
-
       const updatedProject = {
         ...project,
-        tarefaId: project.tarefaId.map((task) => task.id), // Send only task IDs
+        tarefaId: project.tarefaId.map((task) => task.id),
       };
 
       const updatedProjectData = await updateProject(projectId, updatedProject);
-
       onSubmit(updatedProjectData);
     } catch (error) {
       console.error(error);
@@ -25,11 +19,5 @@ export default function EditProjectForm({ projectId, project, onSubmit }) {
     }
   };
 
-  return (
-    <ProjectForm
-      project={project}
-      onSubmit={handleSubmit}
-      onChange={onSubmit}
-    />
-  );
+  return <ProjectForm project={project} onSubmit={handleSubmit} />;
 }
