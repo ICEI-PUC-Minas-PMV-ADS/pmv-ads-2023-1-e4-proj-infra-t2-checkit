73c5e33941a3976ec27a4672d8038f519e4d65cb
feitosinha97 <89053915+feitosinha97@users.noreply.github.com>
2023-06-23 15:46:04 -0300

    Inserindo cadastro e editar usuário

--- front/checkit/src/services/Api.js
+++ front/checkit/src/services/Api.js
@@ -1,14 +1,16 @@
 import axios from "axios";
 import validateToken from "./ValidateToken";
+import { baseURL } from "../../../../MobileApp/src/Services/URL";
+import jwt_decode from "jwt-decode";
 
 const tokenObject = validateToken();
 
 export const api = axios.create({
-  baseURL: "https://checkit-api-gateway-production.up.railway.app/api/",
+  baseURL: "https://checkit-api-gateway-production.up.railway.app/api",
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
     Authorization: `Bearer ${tokenObject.jwtToken}`,
-  }
+  },
 });
 
 export const login = async (email, password) => {
@@ -17,21 +19,31 @@ export const login = async (email, password) => {
   return response.data;
 };
 
-export const register = async (name, email, password, role = 1) => {
-  const response = await api.post("/users", { name, email, password, role });
+export const register = async (name, email, password) => {
+  const response = await api.post(
+    "https://checkit-api-gateway-production.up.railway.app/api/users",
+    { name, email, password }
+  );
   return response.data;
 };
 
 export const updateUser = async (name, password, email) => {
-  const userDataResponse = await api.get("/users");
-  const user = userDataResponse.data.find((user) => user.email === email);
+  const jwtToken = localStorage.getItem("jwtToken");
+
+  // Decode the JWT token to extract the user ID
+  const decodedToken = jwt_decode(jwtToken);
+  console.log(decodedToken)
+  const userId = decodedToken.nameid;
+  const userDataResponse = await api.get(`/users/${userId}`);
+  // const user = userDataResponse.data.find((user) => user.email === email);
+  const user = userDataResponse.data;
 
   if (!user) {
     throw new Error("User not found.");
   }
 
-  const id = user.id;
+  // const id = user.id;
 
-  const response = await api.put(`/users/${id}`, { name, email, password });
+  const response = await api.put(`/users/${userId}`, { name, email, password });
   return response.data;
 };
